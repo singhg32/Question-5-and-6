3f0ed93527c5c88a65f53a16564e2e6f2c06a8be
singhg32 <singhg@stpaulsschool.org.uk>
2022-10-13 15:41:59 +0100

    Add project files.

---
+++ Question 5/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+ int Task1()
+{
+    Console.WriteLine("Enter a whole number");
+    string number1 = Console.ReadLine();
+    int num1 = Convert.ToInt32(number1);
+    Console.WriteLine("Enter another whole number");
+    string number2 = Console.ReadLine();
+    int num2 = Convert.ToInt32(number2);
+    int temp1 = num1;
+    int temp2 = num2;
+    while (temp1 != temp2)
+    {
+        if (temp1 > temp2)
+        {
+            temp1 = temp1 - temp2;
+        }
+        else
+        {
+            temp2 = temp2 - temp1;
+        }
+    }
+    int result = temp1;
+    return (result);
+}
+
+Console.WriteLine(Task1());
+// the while loop was written using temp1 and temp2 so that the original numbers are not altered
---
+++ Question 6/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+string binaryconversion()
+{
+    Console.WriteLine("Enter the number in decimal");
+    string input = Console.ReadLine();
+    int newinput = Convert.ToInt32(input);
+    int temp1 = newinput;
+    List<int> list = new List<int>();
+    while (temp1 > 0)
+    {
+        if (temp1 % 2 == 1)
+        {
+            temp1 = temp1 / 2;
+            Console.WriteLine("1");
+            list.Add(1);
+        }
+        else
+        {
+            temp1 = temp1 / 2;
+            list.Add(0);
+            Console.WriteLine("0");
+        }
+    }
+    
+    string result = string.Join("", list);
+    return (result);
+}
+
+Console.WriteLine(binaryconversion());
